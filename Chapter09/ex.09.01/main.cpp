78ad7b79df77d91f1ce279c5fef5b0e43e93ce3a
lubo3751 <struharlubomir@gmail.com>
2017-05-04 18:34:29 +0200

    Update main.cpp

--- Chapter09/ex.09.01/main.cpp
+++ Chapter09/ex.09.01/main.cpp
@@ -7,40 +7,40 @@ const int MAX = 5;
 
 int main(void)
 {
-	using std:: cout;
-	using std:: endl;
-	golf players[MAX];
-	int counter = 0;  // count entered players
+    using std:: cout;
+    using std:: endl;
+    golf players[MAX];
+    int counter = 0;  // count entered players
 	
-	// prompt user input and set players
-	cout << "Enter names and handicaps of golf players." << endl;
-	cout << "You may enter up to " << MAX 
-		 << " records (empty name stops)." << endl;
-	for (int i = 0; i < MAX; ++i) {
-		cout << i+1 << "." << endl;
-		if (!(setgolf(players[i]))) {
-			break;
-		}
-		++counter;
-	}
+    // prompt user input and set players
+    cout << "Enter names and handicaps of golf players." << endl;
+    cout << "You may enter up to " << MAX 
+         << " records (empty name stops)." << endl;
+    for (int i = 0; i < MAX; ++i) {
+        cout << i+1 << "." << endl;
+        if (!(setgolf(players[i]))) {
+            break;
+        }
+        ++counter;
+    }
 	
-	// show players
-	cout << endl;
-	for (int i = 0; i < counter; ++i) {
-		cout << "golf player " << i+1 << " ";
+    // show players
+    cout << endl;
+    for (int i = 0; i < counter; ++i) {
+        cout << "golf player " << i+1 << " ";
         showgolf(players[i]);
-	}
+    }
 	
-	// alter players
-	setgolf(players[0], "Jan Novak", 26);
-	handicap(players[1], 21);
+    // alter players
+    setgolf(players[0], "Jan Novak", 26);
+    handicap(players[1], 21);
 	
-	// show players
-	cout << endl;
-	for (int i = 0; i < counter; ++i) {
-		cout << "golf player " << i+1 << " ";
+    // show players
+    cout << endl;
+    for (int i = 0; i < counter; ++i) {
+        cout << "golf player " << i+1 << " ";
         showgolf(players[i]);
-	}
+    }
 
-	return 0;
+    return 0;
 }
