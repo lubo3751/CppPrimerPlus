053cc4593575ac5da9b317f754cde0103aa3f4e0
lubo3751 <struharlubomir@gmail.com>
2017-05-04 18:21:37 +0200

    Update main.cpp

--- Chapter09/ex.09.04/main.cpp
+++ Chapter09/ex.09.04/main.cpp
@@ -4,17 +4,21 @@
 
 int main(void)
 {
-	using namespace SALES;
-    Sales s1, s2;
-
+    using SALES::Sales;
+    using SALES::setSales;
+    using SALES::showSales;
+    using SALES::QUARTERS;
+  
+    Sales s1;
     setSales(s1);
     std::cout << "Sales 1";
     showSales(s1);
     
     const double sales[QUARTERS] = {2535.3, 12542.4, 34213.6, 4290}; 
+    Sales s2;
     setSales(s2, sales, QUARTERS);
     std::cout << "Sales 2";
     showSales(s2);
 
-	return 0;
+    return 0;
 }
