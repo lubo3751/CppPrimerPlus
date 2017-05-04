b20d7ba5ea9124baa835f57f387ab90f0f0f4ec5
lubo3751 <struharlubomir@gmail.com>
2017-05-04 18:55:48 +0200

    Update ex.08.04.cpp

--- Chapter08/ex.08.04.cpp
+++ Chapter08/ex.08.04.cpp
@@ -12,8 +12,8 @@ using namespace std;
 #include <cstring> // for strlen(), strcpy()
 
 struct stringy {
-	char * str; // points to a string
-	int ct; // length of string (not counting '\0')
+    char * str; // points to a string
+    int ct; // length of string (not counting '\0')
 };
 
 // prototypes for set(), show(), and show() go here
@@ -23,43 +23,45 @@ void show(const char *, int repeat = 1);
 
 int main()
 {
-	stringy beany;
-	char testing[] = "Reality isn't what it used to be.";
+    stringy beany;
+    char testing[] = "Reality isn't what it used to be.";
 	
-	set(beany, testing); // first argument is a reference,
-	// allocates space to hold copy of testing,
-	// sets str member of beany to point to the
-	// new block, copies testing to new block,
-	// and sets ct member of beany
-	show(beany); // prints member string once
-	show(beany, 2); // prints member string twice
-	testing[0] = 'D';
-	testing[1] = 'u';
-	show(testing); // prints testing string once
-	show(testing, 3); // prints testing string thrice
-	show("Done!");
-	
-	delete beany.str;
-	return 0;
+    set(beany, testing); // first argument is a reference,
+    // allocates space to hold copy of testing,
+    // sets str member of beany to point to the
+    // new block, copies testing to new block,
+    // and sets ct member of beany
+    show(beany); // prints member string once
+    show(beany, 2); // prints member string twice
+    testing[0] = 'D';
+    testing[1] = 'u';
+    show(testing); // prints testing string once
+    show(testing, 3); // prints testing string thrice
+    show("Done!");
+
+    delete beany.str;
+    return 0;
 }
 
 void set(stringy & rStrc, const char ca[])
 {
-	int len = strlen(ca);            // length of testing 
-	char * pca = new char [len+1];   // space to hold copy of testing
-	rStrc.str = pca;                 // str member of beany points to the new block
-	strcpy(pca, ca);        // copy testing to the new block
-	rStrc.ct = len;                  // sets ct member of beany
+    int len = strlen(ca);            // length of testing 
+    char * pca = new char [len+1];   // space to hold copy of testing
+    rStrc.str = pca;                 // str member of beany points to the new block
+    strcpy(pca, ca);        // copy testing to the new block
+    rStrc.ct = len;                  // sets ct member of beany
 }
 
 void show(const stringy & rStrc, int repeat)
 {
-	for (int i = 0; i < repeat; ++i)
-		cout << rStrc.str << endl;
+    for (int i = 0; i < repeat; ++i){
+        cout << rStrc.str << endl;
+    }
 }
 
 void show(const char * s, int repeat)
 {
-	for (int i = 0; i < repeat; ++i)
-		cout << s << endl;
+    for (int i = 0; i < repeat; ++i){
+        cout << s << endl;
+    }
 }
