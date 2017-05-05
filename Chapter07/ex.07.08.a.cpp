28315938e799eecc55e1cdd783aceb4d4892d529
lubo3751 <struharlubomir@gmail.com>
2017-05-05 06:34:49 +0200

    Update ex.07.08.a.cpp

--- Chapter07/ex.07.08.a.cpp
+++ Chapter07/ex.07.08.a.cpp
@@ -11,39 +11,38 @@ array class.)
 #include <iostream>
 #include <string>
 
-const int Seasons = 4;
-const char * Snames[Seasons] =
-    {"Spring", "Summer", "Fall", "Winter"};
+const int SEASONS = 4;
+const char * Snames[SEASONS] = {"Spring", "Summer", "Fall", "Winter"};
 
 // function prototypes
-void fill(double pa[Seasons]);
-void show(double da[Seasons]);
+void fill(double pa[SEASONS]);
+void show(double da[SEASONS]);
 
 int main()
 {
-    double expenses[Seasons];
+    double expenses[SEASONS];
     fill(expenses);
     show(expenses);
     return 0;
 }
 
-void fill(double pa[Seasons])
+void fill(double pa[SEASONS])
 {
-    for (int i = 0; i < Seasons; i++)
+    for (int i = 0; i < SEASONS; i++)
     {
         std::cout << "Enter " << Snames[i] << " expenses: ";
         std::cin >> pa[i];
     }
 }
 
-void show(double da[Seasons])
+void show(double da[SEASONS])
 {
     double total = 0.0;
-    std::cout << "\nEXPENSES\n";
-    for (int i = 0; i < Seasons; i++)
+    std::cout << std::endl << "EXPENSES" << std::endl;
+    for (int i = 0; i < SEASONS; i++)
     {
-        std::cout << Snames[i] << ": $" << da[i] << '\n';
+        std::cout << Snames[i] << ": $" << da[i] << std::endl;
         total += da[i];
     }
-    std::cout << "Total: $" << total << '\n';
+    std::cout << "Total: $" << total << std::endl;
 }
