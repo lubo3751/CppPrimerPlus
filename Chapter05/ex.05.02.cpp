24836cf4f427d7667023b004e9419ac4edb204f8
lubo3751 <struharlubomir@gmail.com>
2017-05-05 10:55:05 +0200

    Update ex.05.02.cpp

--- Chapter05/ex.05.02.cpp
+++ Chapter05/ex.05.02.cpp
@@ -7,24 +7,20 @@ of 100!
 #include <iostream>
 #include <array>
 
-const int ArSize = 101;
+const int ArSIZE = 101;
 
 int main()
 {
-	using namespace std;
-	
-	array<long double, ArSize> factorials;
+    std::array<long double, ArSIZE> factorials;
     factorials[1] = factorials[0] = 1;
-    
-	for (int i = 2; i < ArSize; i++)
-	{
-		factorials[i] = i * factorials[i-1];
-	}
-	
-	for (int i = 0; i < ArSize; i++)
-	{
-		cout << i << "! = " << factorials[i] << endl;
-	}
+
+    for (int i = 2; i < ArSIZE; i++) {
+        factorials[i] = i * factorials[i-1];
+    }
+
+    for (int i = 0; i < ArSIZE; i++) {
+        std::cout << i << "! = " << factorials[i] << std::endl;
+    }
 
     return 0;
 }
