fe6f55cee71e9d48cbd8a5614ea03d1b8df7b956
lubo3751 <struharlubomir@gmail.com>
2017-05-05 11:42:22 +0200

    Update ex.05.09.cpp

--- Chapter05/ex.05.09.cpp
+++ Chapter05/ex.05.09.cpp
@@ -10,20 +10,22 @@ operator to make the comparison test.
 
 int main()
 {
-	using namespace std;
+    using std::cout;
+    using std::cin;
+    using std::endl;
 
-	cout << "Enter words (to stop, type the word done): \n";
-	string word;
-	cin >> word;
+    cout << "Enter words (to stop, type the word done): \n";
+    std::string word;
+    cin >> word;
 
-	int count = 0;
-	while(word != "done")
-	{
-		++count;
-		cin >> word;
-	}
+    int count = 0;
+    while(word != "done")
+    {
+        ++count;
+        cin >> word;
+    }
 
-	cout << "You entered a total of " << count << " words." << endl;	
+    cout << "You entered a total of " << count << " words." << endl;	
 
     return 0;
 }
