552bb52058be2ff512c938c24679ce55753de534
lubo3751 <struharlubomir@gmail.com>
2017-05-05 06:52:06 +0200

    Update ex.07.10.cpp

--- Chapter07/ex.07.10.cpp
+++ Chapter07/ex.07.10.cpp
@@ -7,7 +7,7 @@ calculate(). For example, suppose you have this definition for the add()
 function:
 double add(double x, double y)
 {
-return x + y;
+    return x + y;
 }
 Then, the function call in the following would cause calculate() to pass the values
 2.5 and 10.4 to the add() function and then return the add() return value
@@ -26,8 +26,8 @@ function names as addresses.
 
 #include <iostream>
 
-const int Size = 3;
-const char * Txt[Size] = {" + ", " * ", " - "};  // aids printing results
+const int SIZE = 3;
+const char * Txt[SIZE] = {" + ", " * ", " - "};  // aids printing results
 
 // function prototypes
 double calculate(double, double, double (*pf)(double, double));
@@ -38,40 +38,39 @@ double subtract(double, double);
 int main()
 {
     using namespace std;
-	// array of pointers to functions
-	double (*apf[Size])(double, double) = {add, multiply, subtract};
+    // array of pointers to functions
+    double (*apf[SIZE])(double, double) = {add, multiply, subtract};
 
-	cout << "Enter two numbers: "; 
-	double n1, n2;
-	while (cin >> n1 >> n2) // quits when non-number is entered
-	{
-		for (int i = 0; i < Size; i++) 
-		{   // prints calculations (addition, multiplication, subraction)
-			cout << n1 << Txt[i] << n2 << " = " 
-				 << calculate(n1, n2, apf[i]) << endl;
-		}
-		cout << "Enter another two numbers <q to terminate>: ";
-	}
-	cout << "Bye\n";    
+    cout << "Enter two numbers: "; 
+    double n1, n2;
+    while (cin >> n1 >> n2) // quits when non-number is entered
+    {
+        // prints calculations (addition, multiplication, subraction)
+        for (int i = 0; i < SIZE; i++) {   
+            cout << n1 << Txt[i] << n2 << " = " << calculate(n1, n2, apf[i]) << endl;
+        }
+        cout << "Enter another two numbers <q to terminate>: ";
+    }
+    cout << "Bye" << endl;
     return 0;
 }
 
 double calculate(double x, double y, double (*pf)(double, double))
 {
-	return pf(x, y);
+    return pf(x, y);
 }
 
 double add(double x, double y)
 {
-	return x + y;
+    return x + y;
 }
 
 double multiply(double x, double y)
 {
-	return x * y;
+    return x * y;
 }
 
 double subtract(double x, double y)
 {
-	return x - y;
+    return x - y;
 }
