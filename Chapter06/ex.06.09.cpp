6c6c3f865f751356eedb2090e7aa8b5dd5bf3c8d
lubo3751 <struharlubomir@gmail.com>
2017-05-05 10:40:33 +0200

    Update ex.06.09.cpp

--- Chapter06/ex.06.09.cpp
+++ Chapter06/ex.06.09.cpp
@@ -18,64 +18,67 @@ Rich Raptor
 #include <string>
 #include <fstream>          // file I/O support
 #include <cstdlib>          // support for exit()
-using namespace std;
 
-const int Limit = 10000;
+const int LIMIT = 10000;
 
 struct donor {
-	string name;
-	double amount;
+    std::string name;
+    double amount;
 };
 
 int main()
 {
-	char filename[] = "donors.txt";
-    ifstream inFile;        // object for handling file input
-	inFile.open(filename);  // associate inFile with a file
+    using std::cout;
+    using std::endl;
+    char file_name[] = "donors.txt";
+    std::ifstream inFile;        // object for handling file input
+    inFile.open(file_name);  // associate inFile with a file
 
     if (!inFile.is_open())  // check the file has opened
     {
-        cout << "Could not open the file " << filename << endl;
-        cout << "Program terminating.\n";
+        cout << "Could not open the file " << file_name << endl;
+        cout << "Program terminating." << endl;
         exit(EXIT_FAILURE);
     }
 
-	int n_donors;
-	(inFile >> n_donors).get();  // read how many donors
-	
-	// dynamically allocate array of structures
-	donor *donors = new donor[n_donors];
+    int num_of_donors;
+    (inFile >> num_of_donors).get();  // read how many donors
 
-	// initialize structure
-	for (int i = 0; i < n_donors; ++i) 
-	{
-		getline(inFile, donors[i].name);    // reads donor's name
-		(inFile >> donors[i].amount).get(); // reads donor's contribution
-	}
+    // dynamically allocate array of structures
+    donor * pt_donors = new donor[num_of_donors];
 
-	// display Grand Patrons
-	cout << "Grand Patrons:\n";
-	int counter = 0;
-	for (int i = 0; i < n_donors; ++i) {
-		if (donors[i].amount >= Limit) {
-			cout << donors[i].name << " donated $" << donors[i].amount << endl;
-			++counter;
-		}
-	}
-	if (!counter)
-		cout << "none\n";
+    // initialize structure
+    for (int i = 0; i < num_of_donors; ++i) 
+    {
+        getline(inFile, pt_donors[i].name);    // reads donor's name
+        (inFile >> pt_donors[i].amount).get(); // reads donor's contribution
+    }
 
-	// display Patrons
-	cout << "Patrons:\n";
-	counter = 0;
-	for (int i = 0; i < n_donors; ++i) {
-		if (donors[i].amount < Limit) {
-			cout << donors[i].name << endl;
-			++counter;
-		}
-	}
-	if (!counter)
-		cout << "none\n";
+    // display Grand Patrons
+    cout << "Grand Patrons:" << endl;
+    int counter = 0;
+    for (int i = 0; i < num_of_donors; ++i) {
+        if (pt_donors[i].amount >= LIMIT) {
+            cout << pt_donors[i].name << " donated $" << pt_donors[i].amount << endl;
+            ++counter;
+        }
+    }
+    if (!counter) {
+        cout << "none" << endl;
+    }
+
+    // display Patrons
+    cout << "Patrons:" << endl;
+    counter = 0;
+    for (int i = 0; i < num_of_donors; ++i) {
+        if (pt_donors[i].amount < LIMIT) {
+            cout << pt_donors[i].name << endl;
+            ++counter;
+        }
+    }
+    if (!counter) {
+        cout << "none" << endl;
+    }
 
     return 0;
 }
