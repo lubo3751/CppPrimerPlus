765944941f8dedd8dc85e19c7c31c854836214b8
lubo3751 <struharlubomir@gmail.com>
2017-05-05 06:45:12 +0200

    Update ex.07.09.cpp

--- Chapter07/ex.07.09.cpp
+++ Chapter07/ex.07.09.cpp
@@ -10,9 +10,9 @@ using namespace std;
 const int SLEN = 30;
 
 struct student {
-	char fullname[SLEN];
-	char hobby[SLEN];
-	int ooplevel;
+    char fullname[SLEN];
+    char hobby[SLEN];
+    int ooplevel;
 };
 
 // function prototypes
@@ -23,23 +23,24 @@ void display3(const student pa[], int n);
 
 int main()
 {
-	cout << "Enter class size: ";
-	int class_size;
-	cin >> class_size;
-	while (cin.get() != '\n')
-		continue;
-	
-	student * ptr_stu = new student[class_size];
-	int entered = getinfo(ptr_stu, class_size);
-	for (int i = 0; i < entered; i++)
-	{
-		display1(ptr_stu[i]);
-		display2(&ptr_stu[i]);
-	}
-	display3(ptr_stu, entered);
-	delete [] ptr_stu;
-	cout << "Done\n";
-	return 0;
+    cout << "Enter class size: ";
+    int class_size;
+    cin >> class_size;
+    while (cin.get() != '\n') {
+        continue;
+    }
+
+    student * ptr_stu = new student[class_size];
+    int entered = getinfo(ptr_stu, class_size);
+    for (int i = 0; i < entered; i++)
+    {
+        display1(ptr_stu[i]);
+        display2(&ptr_stu[i]);
+    }
+    display3(ptr_stu, entered);
+    delete [] ptr_stu;
+    cout << "Done" << endl;
+    return 0;
 }
 
 // getinfo() has two arguments: a pointer to the first element of
@@ -51,41 +52,43 @@ int main()
 // filled.
 int getinfo(student pa[], int n)
 {
-	int i;
-	for (i = 0; i < n; i++)
-	{
-		cout << "Enter the full name of the student #" << i + 1 << ": ";
-		cin.getline((pa+i)->fullname, SLEN);
-		if (((pa+i)->fullname)[0] == '\0')
-			break;
-
-		cout << "Enter a hobby of student #" << i + 1 << ": ";
-		cin.getline((pa+i)->hobby, SLEN);
-		
-		cout << "Enter the ooplevel of student #" << i + 1 << ": ";
-		cin >> (pa+i)->ooplevel;
-		while (cin.get() != '\n')
-			continue;
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        cout << "Enter the full name of the student #" << i + 1 << ": ";
+        cin.getline((pa+i)->fullname, SLEN);
+        if (((pa+i)->fullname)[0] == '\0') {
+            break;
 	}
-	return i;
+
+        cout << "Enter a hobby of student #" << i + 1 << ": ";
+        cin.getline((pa+i)->hobby, SLEN);
+
+        cout << "Enter the ooplevel of student #" << i + 1 << ": ";
+        cin >> (pa+i)->ooplevel;
+        while (cin.get() != '\n') {
+            continue;
+	}
+    }
+    return i;
 }
 
 // display1() takes a student structure as an argument
 // and displays its contents
 void display1(student st)
 {
-	cout << "name: " << st.fullname << ", ";
-	cout << "hobby: " << st.hobby << ", ";
-	cout << "ooplevel: " << st.ooplevel << endl;
+    cout << "name: " << st.fullname << ", ";
+    cout << "hobby: " << st.hobby << ", ";
+    cout << "ooplevel: " << st.ooplevel << endl;
 }
 
 // display2() takes the address of student structure as an
 // argument and displays the structure’s contents
 void display2(const student * ps)
 {
-	cout << "name: " << ps->fullname << ", ";
-	cout << "hobby: " << ps->hobby << ", ";
-	cout << "ooplevel: " << ps->ooplevel << endl;
+    cout << "name: " << ps->fullname << ", ";
+    cout << "hobby: " << ps->hobby << ", ";
+    cout << "ooplevel: " << ps->ooplevel << endl;
 }
 
 // display3() takes the address of the first element of an array
@@ -93,10 +96,10 @@ void display2(const student * ps)
 // arguments and displays the contents of the structures
 void display3(const student pa[], int n)
 {
-	for (int i = 0; i < n; i++)
-	{
-		cout << "name: " << (pa+i)->fullname << ", ";
-		cout << "hobby: " << (pa+i)->hobby << ", ";
-		cout << "ooplevel: " << (pa+i)->ooplevel << endl;
-	}
+    for (int i = 0; i < n; i++)
+    {
+        cout << "name: " << (pa+i)->fullname << ", ";
+        cout << "hobby: " << (pa+i)->hobby << ", ";
+        cout << "ooplevel: " << (pa+i)->ooplevel << endl;
+    }
 }
