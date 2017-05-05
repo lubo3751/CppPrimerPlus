9099d040b52c60d47d56bd037f7a211ad4c84b1d
lubo3751 <struharlubomir@gmail.com>
2017-05-05 05:30:57 +0200

    Update ex.07.01.cpp

--- Chapter07/ex.07.01.cpp
+++ Chapter07/ex.07.01.cpp
@@ -14,19 +14,19 @@ double harmonicMean(double, double);
 int main()
 {
     using namespace std;
-	cout << "Enter two numbers (0 to quit): ";
-	double num1, num2;
+    cout << "Enter two numbers (0 to quit): ";
+    double num1, num2;
 
-	while (cin >> num1 >> num2 && num1 != 0 && num2 != 0)
-	{
-		cout << "Their Harmonic Mean is " << harmonicMean(num1, num2);
-		cout << "\nEnter two numbers (0 to quit): ";
-	}
-	cout << "Bye!\n";
+    while (cin >> num1 >> num2 && num1 != 0 && num2 != 0)
+    {
+        cout << "Their Harmonic Mean is " << harmonicMean(num1, num2);
+        cout << "\nEnter two numbers (0 to quit): ";
+    }
+    cout << "Bye!\n";
     return 0;
 }
 
 double harmonicMean(double n1, double n2)
 {
-	return 2.0 * n1 * n2 / (n1 + n2); 
+    return 2.0 * n1 * n2 / (n1 + n2); 
 }
