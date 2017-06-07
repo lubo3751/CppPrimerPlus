a0ed1c1b7bcdbc97a6452953e63abbcccb6c8084
Lubomir Struhar <lubo3751@gmail.com>
2017-06-07 14:09:42 +0200

    indentation improved

--- Chapter12/ex.12.03/stock20.cpp
+++ Chapter12/ex.12.03/stock20.cpp
@@ -8,7 +8,7 @@ Stock::Stock()     // default constructor
     len = 8;
     company = new char[len];
     strcpy_s(company, len, "no name");
-	shares = 0;
+    shares = 0;
     share_val = 0.0;
     total_val = 0.0;
 }
@@ -21,7 +21,7 @@ Stock::Stock(const char * co, long n, double pr)
     if (n < 0)
     {
         std::cout << "Number of shares can't be negative; "
-                   << company << " shares set to 0.\n";
+                  << company << " shares set to 0.\n";
         shares = 0;
     }
     else
@@ -53,7 +53,7 @@ void Stock::buy(long num, double price)
      if (num < 0)
     {
         std::cout << "Number of shares purchased can't be negative. "
-             << "Transaction is aborted.\n";
+                  << "Transaction is aborted.\n";
     }
     else
     {
@@ -95,7 +95,7 @@ const Stock & Stock::topval(const Stock & s) const
     if (s.total_val > total_val)
         return s;
     else
-        return *this; 
+        return *this;
 }
 
 // overloaded operator method
@@ -120,8 +120,8 @@ ostream & operator<<(ostream & os, Stock s)
     using std::ios_base;
     // set format to #.###
     ios_base::fmtflags orig = 
-        os.setf(ios_base::fixed, ios_base::floatfield); 
-    std::streamsize prec = os.precision(3);    
+        os.setf(ios_base::fixed, ios_base::floatfield);
+    std::streamsize prec = os.precision(3);
     
     os << "Company: " << s.company
         << "  Shares: " << s.shares << '\n';
